74d66a0c58d6c8cef324d14801fbd387adeeb5bb
businesskaniki <businesskanikii@gmail.com>
2022-10-04 16:56:22 +0300

    corecting linters

--- person.rb
+++ person.rb
@@ -1,43 +1,42 @@
 class Person
-    def initialize(age, name = 'Unknown', parent_permission: true)
-      @id = Random.rand(1..1000)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    public
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  
-  class Student < Person
-    def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-      super(age, name, parent_permission)
-      @classroom = classroom
-    end
-  
-    def play_hooky
-      "¯\(ツ)/¯"
-    end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
-  
-  class Teacher < Person
-    def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-      super(age, name, parent_permission)
-      @specialization = specialization
-    end
-  
-    def can_use_services?
-      true
-    end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
-  
+end
+
+class Student < Person
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
